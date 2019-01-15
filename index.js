0ef231f61917a196403cf27c2bc732029c282ab1
tarinalee <tarinalee@gmail.com>
2019-01-15 18:56:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
- return string.toLowerCase() 
+ return 'I cant\'t hear you!'.toLowerCase() 
 }
 function sayHiToGrandma(string) {
   return string.toUpperCase()
